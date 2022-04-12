71270b99f9aedce9f4fb8869e31c91c338534143
Elijah Amian <eramian125@gmail.com>
2022-04-12 14:02:26 -0700

    Update Engine.js

--- Source/Engine.js
+++ Source/Engine.js
@@ -13,17 +13,11 @@ const Engine = function(time_step, update, render) {
 
   this.run = function(time_stamp) {
 
-    /* I moved this line from the bottom of this function to the top. This is better
-    anyway, because it ensures that if my game logic runs too long, a new frame will
-    already be requested before 30 or 60 frames pass and I miss a request entirely.
-    This could cause a "spiral of death" for my CPU, but since I have the frame dropping
-    safety if statement, this probably won't crash the user's computer. */
     this.animation_frame_request = window.requestAnimationFrame(this.handleRun);
 
     this.accumulated_time += time_stamp - this.time;
     this.time = time_stamp;
 
-    /* This is the safety if statement. */
     if (this.accumulated_time >= this.time_step * 3) {
 
       this.accumulated_time = this.time_step;
